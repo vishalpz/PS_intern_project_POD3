69367823fb629d36425d1e03b1867cb7e9d05066
Julia Kolano <julkolan@WKMZTBDA2259.global.publicisgroupe.net>
2023-07-25 17:25:01 +0100

    initial main page

--- asda_gpt/src/App.js
+++ asda_gpt/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import background from "./images/ASDA_background.png";
+import chatbotIcon from "./images/Chatbot_icon.png";
+import "./App.css";
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="Background" src={background} alt="Main Page of ASDA"></img>
+      <a href="" rel="noreferrer">
+        <img className="ChatbotIcon" src={chatbotIcon} alt="chatbot icon"></img>
+        {/* Chatbot icon link: https://icons8.com/icons/set/chat-bot--static--green */}
+      </a>
     </div>
   );
 }
