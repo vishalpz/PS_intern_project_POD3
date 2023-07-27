48ed498d14c690a4aa181f4ebfc712592d827b61
Julia Kolano <julkolan@WKMZTBDA2259.global.publicisgroupe.net>
2023-07-27 15:46:14 +0100

    initial Login Sign Up Guest Page

--- asda_gpt/src/App.js
+++ asda_gpt/src/App.js
@@ -5,14 +5,18 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppMain">
       <img
-        className="Background"
+        className="BackgroundMain"
         src={background}
         alt="Main Page of ASDA"
       ></img>
       <Link to="/LoginOrGuest">
-        <img className="ChatbotIcon" src={chatbotIcon} alt="chatbot icon"></img>
+        <img
+          className="ChatbotIconMain"
+          src={chatbotIcon}
+          alt="chatbot icon"
+        ></img>
         {/* Chatbot icon link: https://icons8.com/icons/set/chat-bot--static--green */}
       </Link>
     </div>
--- asda_gpt/src/pages/LoginGuest.js
+++ asda_gpt/src/pages/LoginGuest.js
@@ -1,16 +1,94 @@
 import background from "../images/ASDA_background.png";
-import "../styles/App.css";
+import chatbotIcon from "../images/Chatbot_icon.png";
+import close from "../images/Close.png";
+import "../styles/LoginGuest.css";
+import { Link } from "react-router-dom";
 
 function LoginGuest() {
   return (
     <div className="App">
+      <Background>
+        <Box>
+          <LoginBox>
+            <Login></Login>
+            <SignUp></SignUp>
+            <Guest></Guest>
+          </LoginBox>
+        </Box>
+      </Background>
+    </div>
+  );
+}
+
+function Background({ children, ...props }) {
+  return (
+    <div {...props}>
       <img
         className="Background"
         src={background}
         alt="Main Page of ASDA"
       ></img>
+      <img className="ChatbotIcon" src={chatbotIcon} alt="chatbot icon"></img>
+      {children}
     </div>
   );
+}
+
+function Box({ children, ...props }) {
+  return (
+    <div className="Box" {...props}>
+      <Link to="/">
+        <img className="Close" src={close} alt="close icon"></img>
+        {/* Close icon: https://icons8.com/icons/set/close--static--middle-gray */}
+      </Link>
+      {children}
+    </div>
+  );
+}
+
+function LoginBox({ children, ...props }) {
+  return (
+    <div className="LoginBox" {...props}>
+      {children}
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1 className="LoginHeading">Login</h1>
+      <form>
+        <label>
+          <input
+            type="email"
+            placeholder="Enter Email Address"
+            className="InputBox"
+          ></input>
+        </label>
+        <label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="InputBox"
+          ></input>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+function SignUp() {
+  return (
+    <div>
+      <h1 className="SignUpHeading">New User?</h1>
+      <button className="SignUpButton">Sign up Now</button>
+    </div>
+  );
+}
+
+function Guest() {
+  return <h2 className="GuestHeading">Continue as a guest</h2>;
 }
 
 export default LoginGuest;
