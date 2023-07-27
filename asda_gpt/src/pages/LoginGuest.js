7bd0f4ab64be8725998e6fd75830a88c080d157b
tyanatsh <tyana3416@gmail.com>
2023-07-27 12:06:05 +0100

    made background image global

--- asda_gpt/src/pages/LoginGuest.js
+++ asda_gpt/src/pages/LoginGuest.js
@@ -1,16 +1,11 @@
-import background from "../images/ASDA_background.png";
 import "../styles/App.css";
 
 function LoginGuest() {
   return (
     <div className="App">
-      <img
-        className="Background"
-        src={background}
-        alt="Main Page of ASDA"
-      ></img>
+      
     </div>
   );
 }
-
+// removed background image and put it in App.css
 export default LoginGuest;
