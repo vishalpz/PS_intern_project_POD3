db5d99c7716f8c61ea9cff4c0a97f856ddb795f5
Julia Kolano <julkolan@WKMZTBDA2259.global.publicisgroupe.net>
2023-07-31 12:29:22 +0100

    got user data from input fields

--- asda_gpt/src/pages/SignUp.js
+++ asda_gpt/src/pages/SignUp.js
@@ -4,6 +4,7 @@ import background from "../images/ASDA_background.png";
 import chatbotIcon from "../images/Chatbot_icon.png";
 import WhiteBox from "../comps/WhiteBox";
 import CloseIcon from "../comps/Close";
+import {useState} from "react";
 
 function SignUp() {
   return (
@@ -18,6 +19,8 @@ function SignUp() {
     </div>
   );
 }
+
+// COMPONENTS
 
 function Background({ children, ...props }) {
   return (
@@ -46,31 +49,88 @@ function SignUpHeading() {
 }
 
 function SignUpForm() {
+
+    // FORM LOGIC
+
+    const [input, setInput] = useState({
+      name: '',
+      email: '',
+      password: '',
+      store: '',
+      diet: ''
+    })
+    
+    function handleChange(event){
+      const {name, value} = event.target;
+  
+      setInput(prevInput => {
+        return {
+          ...prevInput,
+          [name]: value
+        }
+      })
+    }
+  
+    function handleClick(event) {
+      event.preventDefault();
+      console.log(input);
+    }
+
   return (
     <div>
       <form>
         <label className="Label">
           Name
-          <input type="text" className="InputBox"></input>
+          <input
+            name="name"
+            value={input.name}
+            type="text"
+            className="InputBox"
+            onChange={handleChange}
+          ></input>
         </label>
         <label className="Label">
           Email Address
-          <input type="email" className="InputBox"></input>
+          <input
+            name="email"
+            value={input.email}
+            type="email"
+            className="InputBox"
+            onChange={handleChange}
+          ></input>
         </label>
         <label className="Label">
           Password
-          <input type="password" className="InputBox"></input>
+          <input
+            name="password"
+            value={input.password}
+            type="password"
+            className="InputBox"
+            onChange={handleChange}
+          ></input>
         </label>
         <label className="Label">
           Choose Store
-          <input type="text" className="InputBox"></input>
+          <input
+            name="store"
+            value={input.store}
+            type="text"
+            className="InputBox"
+            onChange={handleChange}
+          ></input>
         </label>
         <label className="Label">
           Dietary Requirements
-          <input type="text" className="InputBox"></input>
+          <input
+            name="diet"
+            value={input.diet}
+            type="text"
+            className="InputBox"
+            onChange={handleChange}
+          ></input>
         </label>
       </form>
-      <button type="submit" className="SignUpButton2">
+      <button className="SignUpButton2" onClick={handleClick}>
         Sign Up
       </button>
     </div>
