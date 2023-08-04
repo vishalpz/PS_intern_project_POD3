40bcea3f2a2856e1381397a414bd037817a5c9c8
Julia Kolano <julkolan@WKMZTBDA2259.global.publicisgroupe.net>
2023-08-04 10:43:42 +0100

    fixing2

--- asda_gpt/src/pages/ChatBot.js
+++ asda_gpt/src/pages/ChatBot.js
@@ -17,6 +17,13 @@ function ChatBot() {
           <CloseIcon />
           <ChatArea>
             <ChatResponse />
+            <UserQuestion />
+            <ChatResponse />
+            <UserQuestion />
+            <ChatResponse />
+            <UserQuestion />
+            <ChatResponse />
+            <UserQuestion />
           </ChatArea>
           <InputArea />
         </WhiteBox>
@@ -49,6 +56,9 @@ function InputArea() {
       <img className="VoiceImage" src={VoiceIcon} alt="mic icon"></img>
       {/* microphone icon link: https://icons8.com/icon/set/microphone/fluency-systems-regular--static--green */}
       <form className="Form">
+      <form className="Form">
+        <img className="VoiceImage" src={VoiceIcon} alt="mic icon"></img>
+        {/* microphone icon link: https://icons8.com/icon/set/microphone/fluency-systems-regular--static--green */}
         <label>
           <input
             type="text"
@@ -76,6 +86,29 @@ function ChatResponse() {
     <div>
       <img className="ChatBotIcon2" src={ChatbotIcon2} alt="chatbot icon"></img>
       {/* Chatbot icon link: https://icons8.com/icons/set/bot--static--green */}
+      <p className="Message">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      </p>
+    </div>
+  );
+}
+
+function UserQuestion() {
+  return (
+    <div className="UserQuestion">
+      <img className="UserIcon" src={UserIcon} alt="user icon"></img>
+      <p className="Question">
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+        {/* There can also be images in the text bubbles */}
+        {/* <img className="Image" src={background}></img> */}
+      </p>
     </div>
   );
 }
