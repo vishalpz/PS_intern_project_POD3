3ac1d5cee49f73c5daaf2abd5ed24653a980c1ae
tyanatsh <tyana3416@gmail.com>
2023-08-04 10:28:21 +0100

    Revert "prompt page"
    This reverts commit c442de3b39fbce83373aae0a11eac57a52300bfe.

--- asda_gpt/src/pages/Loading1.js
+++ asda_gpt/src/pages/Loading1.js
@@ -1,23 +1,16 @@
 import background from "../images/ASDA_background.png";
 import chatbotIcon from "../images/Chatbot_icon.png";
-import WhiteBox from "../comps/WhiteBox";
-import CloseIcon from "../comps/Close";
-import VoiceIcon from "../images/Microphone.png";
-import SubmitIcon from "../images/Submit.png";
-import PromptButtonContainer from "../containers/PromptButtonContainer";
 import "../styles/ChatBot.css";
-import "../styles/App.css";
+import WhiteBox from '../comps/WhiteBox';
+import CloseIcon from '../comps/Close'; 
 
 function Loading1() {
   return (
     <div className="App">
       <Background1>
         <WhiteBox>
-          <CloseIcon />
-          <h1 className="Load1Heading">Chat with ASDA GPT</h1>
-        <PromptButtonContainer/>
-        <p>or...</p>
-          <InputArea />
+        <CloseIcon/><p>hi jane!</p>
+        <InputArea />
         </WhiteBox>
       </Background1>
     </div>
@@ -32,21 +25,15 @@ function Background1({ children, ...props }) {
         src={background}
         alt="Main Page of ASDA"
       ></img>
-      <img
-        className="ChatbotIconBlur"
-        src={chatbotIcon}
-        alt="chatbot icon"
-      ></img>
+      <img className="ChatbotIconBlur" src={chatbotIcon} alt="chatbot icon"></img>
       {children}
     </div>
   );
 }
-
 function InputArea() {
   return (
     <div className="InpuArea">
-      <img className="VoiceImage" src={VoiceIcon} alt="mic icon"></img>
-      {/* microphone icon link: https://icons8.com/icon/set/microphone/fluency-systems-regular--static--green */}
+      <img className="VoiceImage" alt="mic icon"></img>
       <form className="Form">
         <label>
           <input
@@ -54,12 +41,10 @@ function InputArea() {
             placeholder="Ask something..."
             className="ChatInput"
           ></input>
-          <input type="image" src={SubmitIcon} className="SubmitImage" alt="submit icon"></input>
         </label>
       </form>
-      {/* submit icon link: https://icons8.com/icon/set/submit-arrow/fluency-systems-regular--static--green */}
+      <img className="SubmitImage" alt="submit icon"></img>
     </div>
   );
 }
-
 export default Loading1;
